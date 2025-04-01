9fec903a10abc435f6754a1711079b3a3455aaf7
王博宁 <2953511584@qq.com>
2025-04-01 16:28:28 +0800

    因为更改了按键，改变了扫键方式

--- Key/Scankey_Normal.asm
+++ Key/Scankey_Normal.asm
@@ -5,160 +5,187 @@ L_Scankey_usually_Prog:
 	STA		P_Scankey_value_Temporary
 	LDA		P_PA
 	AND		#D_PA_Press
-	CMP		#D_PA_Press
+	CMP		#D_PA_Value
 	BNE		L_Scankey_usually_Prog_Countine;不相等跳转
+L_Scankey_usually_Prog_RTS:
 	RTS
 	
 L_Scankey_usually_Prog_Countine:
-	STA		P_Temp
+	STA		P_Temp+1
 	CMP		#D_PA2_Press
 	BEQ		L_Scankey_usually_Prog_Countine_PA2
 	CMP		#D_PA3_Press
 	BEQ		L_Scankey_usually_Prog_Countine_PA3
+	CMP		#D_PA5_PA6_Press
+	BEQ		L_Scankey_usually_Prog_Countine_PA5_PA6
+	JSR		L_PC_Output_High_Prog
+	BBS5	Sys_Flag_A,L_Scankey_usually_Prog_RTS
+	LDA		P_Temp+1
 	CMP		#D_PA4_Press
 	BEQ		L_Scankey_usually_Prog_Countine_PA4
 	CMP		#D_PA5_Press
 	BEQ		L_Scankey_usually_Prog_Countine_PA5
 	CMP		#D_PA6_Press
 	BEQ		L_Scankey_usually_Prog_Countine_PA6
-L_Scankey_Effictive_Prog:	
+	
+	RTS
+
+L_Scankey_usually_Prog_Countine_PA2:
+	LDA		#D_B_Press
+	STA		P_Scankey_value_Temporary
+	RTS
+
+L_Scankey_usually_Prog_Countine_PA3:
+	LDA		#D_C_Press
+	STA		P_Scankey_value_Temporary
+	RTS
+L_Scankey_usually_Prog_Countine_PA5_PA6:
+	JSR		L_PC_Output_High_Prog
+	LDA		P_Temp+3
+	ORA		P_Temp+5
+	ORA		P_Temp+6
+	ORA		P_Temp+8
+	BNE		L_Scankey_Effictive_Prog
+	LDA		P_Temp+4	
+	BEQ		L_Scankey_Effictive_Prog
+	LDA		P_Temp+7
+	BEQ		L_Scankey_Effictive_Prog
+	LDA		#D_Beep_Test_Press
+	STA		P_Scankey_value_Temporary
+	RTS
+
+L_Scankey_usually_Prog_Countine_PA4:
+	CLC
+	LDA		#0
+	ADC		P_Temp+2
+	STA		P_Scankey_value_Temporary
+	RTS
+
+L_Scankey_usually_Prog_Countine_PA5:
+	CLC
+	LDA		#7
+	ADC		P_Temp+2
+	STA		P_Scankey_value_Temporary
+	RTS
+
+L_Scankey_usually_Prog_Countine_PA6:
+	CLC
+	LDA		#14
+	ADC		P_Temp+2
+	STA		P_Scankey_value_Temporary
+	RTS
+L_Scankey_Effictive_Prog:
 	SMB5	Sys_Flag_A
 	LDA		#0
 	STA		P_Scankey_value_Temporary
 	RTS
 
-L_Scankey_usually_Prog_Countine_PA3:
-	LDA		#20
-	STA		P_Scankey_value_Temporary
+L_PC_Output_High_Prog:
 	JSR		L_PA_Intput_Low_Prog
-
 	JSR		L_PC0_Output_High_Prog
 	JSR		L_ScanKey_Delay_Prog
+
 	LDA		P_PA
 	AND		#D_PA_Press
 	STA		P_Temp+3
 
 	JSR		L_PC1_Output_High_Prog
 	JSR		L_ScanKey_Delay_Prog
-	LDA		P_PA
-	AND		#D_PA_Press
-	STA		P_Temp+4
 
-	JSR		L_Scankey_Effictive_Init;PA口输入下拉,PC口输出低
-
-	LDA		P_Temp+3
-	BNE		L_Scankey_usually_Prog_Countine_PA3_PC0
-	LDA		P_Temp+4
-	BEQ		L_Scankey_usually_Prog_Countine_PA3_RTS
-	LDA		#22
-	STA		P_Scankey_value_Temporary
-	RTS
-	
-
-L_Scankey_usually_Prog_Countine_PA3_PC0:
-	LDA		P_Temp+4
-	BNE		L_Scankey_usually_Prog_Countine_PA3_RTS
-	LDA		#21
-	STA		P_Scankey_value_Temporary
-	RTS
-
-L_Scankey_usually_Prog_Countine_PA3_RTS:
-	JMP		L_Scankey_Effictive_Prog
-
-L_Scankey_usually_Prog_Countine_PA2:
-	LDA		#0
-	STA		P_Scankey_value_Temporary
-	BRA		L_Scankey_usually_Prog_Countine_1
-
-L_Scankey_usually_Prog_Countine_PA4:
-	LDA		#5
-	STA		P_Scankey_value_Temporary
-	BRA		L_Scankey_usually_Prog_Countine_1
-L_Scankey_usually_Prog_Countine_PA5:
-	LDA		#10
-	STA		P_Scankey_value_Temporary
-	BRA		L_Scankey_usually_Prog_Countine_1
-L_Scankey_usually_Prog_Countine_PA6:
-	LDA		#15
-	STA		P_Scankey_value_Temporary
-
-L_Scankey_usually_Prog_Countine_1:
-
-	JSR		L_PA_Intput_Low_Prog
-
-	JSR		L_PC0_Output_High_Prog
-	JSR		L_ScanKey_Delay_Prog
-	LDA		P_PA
-	AND		#D_PA_Press
-	STA		P_Temp+3
-
-	JSR		L_PC1_Output_High_Prog
-	JSR		L_ScanKey_Delay_Prog
 	LDA		P_PA
 	AND		#D_PA_Press
 	STA		P_Temp+4
 
 	JSR		L_PC2_Output_High_Prog
 	JSR		L_ScanKey_Delay_Prog
+
 	LDA		P_PA
 	AND		#D_PA_Press
 	STA		P_Temp+5
 
 	JSR		L_PC3_Output_High_Prog
 	JSR		L_ScanKey_Delay_Prog
+
 	LDA		P_PA
 	AND		#D_PA_Press
 	STA		P_Temp+6
 
-	JSR		L_Scankey_Effictive_Init;PA口输入下拉,PC口输出低
+	JSR		L_PC4_Output_High_Prog
+	JSR		L_ScanKey_Delay_Prog
+
+	LDA		P_PA
+	AND		#D_PA_Press
+	STA		P_Temp+7
+
+	JSR		L_PC5_Output_High_Prog
+	JSR		L_ScanKey_Delay_Prog
+
+	LDA		P_PA
+	AND		#D_PA_Press
+	STA		P_Temp+8
+
+
+
+	JSR		L_Scankey_Effictive_Init
 
 	LDA		P_Temp+3
-	BNE		L_Scankey_usually_Prog_Countine_PC0
+	BNE		L_PC_Output_High_Prog_PC0
 	LDA		P_Temp+4
-	BNE		L_Scankey_usually_Prog_Countine_PC1
+	BNE		L_PC_Output_High_Prog_PC1
 	LDA		P_Temp+5
-	BNE		L_Scankey_usually_Prog_Countine_PC2
+	BNE		L_PC_Output_High_Prog_PC2
 	LDA		P_Temp+6
-	BNE		L_Scankey_usually_Prog_Countine_PC3
-	JMP		L_Scankey_Effictive_Prog
+	BNE		L_PC_Output_High_Prog_PC3
+	LDA		P_Temp+7
+	BNE		L_PC_Output_High_Prog_PC4
+	LDA		P_Temp+8
+	BNE		L_PC_Output_High_Prog_PC5
+	RTS
 
-
-
-
-
-L_Scankey_usually_Prog_Countine_PC0:
-	LDA		#1
-	STA		P_Temp
+L_PC_Output_High_Prog_PC0:
 	LDA		P_Temp+4
 	ORA		P_Temp+5
 	ORA		P_Temp+6
-	BEQ		L_Scankey_usually_Prog_Countine_RTS
-	JMP		L_Scankey_Effictive_Prog
-	
-L_Scankey_usually_Prog_Countine_PC1:
-	LDA		#2
-	STA		P_Temp
+	ORA		P_Temp+7
+	ORA		P_Temp+8
+	BNE		L_Scankey_Effictive_Prog_To
+	LDA		#1
+	STA		P_Temp+2
+	RTS
+L_PC_Output_High_Prog_PC1:
 	LDA		P_Temp+5
 	ORA		P_Temp+6
-	BEQ		L_Scankey_usually_Prog_Countine_RTS
-	JMP		L_Scankey_Effictive_Prog
-
-
-L_Scankey_usually_Prog_Countine_PC2:
-	LDA		#3
-	STA		P_Temp
-	LDA		P_Temp+6
-	BEQ		L_Scankey_usually_Prog_Countine_RTS
-	JMP		L_Scankey_Effictive_Prog
-
-L_Scankey_usually_Prog_Countine_PC3:
-	LDA		#4
-	STA		P_Temp
-
-L_Scankey_usually_Prog_Countine_RTS:
-	CLC
-	LDA		P_Temp
-	ADC		P_Scankey_value_Temporary
-	STA		P_Scankey_value_Temporary
+	ORA		P_Temp+7
+	ORA		P_Temp+8
+	BNE		L_Scankey_Effictive_Prog_To
+	LDA		#2
+	STA		P_Temp+2
 	RTS
-
+L_PC_Output_High_Prog_PC2:
+	LDA		P_Temp+6
+	ORA		P_Temp+7
+	ORA		P_Temp+8
+	BNE		L_Scankey_Effictive_Prog_To
+	LDA		#3
+	STA		P_Temp+2
+	RTS
+	
+L_PC_Output_High_Prog_PC3:
+	LDA		P_Temp+7
+	ORA		P_Temp+8
+	BNE		L_Scankey_Effictive_Prog_To
+	LDA		#4
+	STA		P_Temp+2
+	RTS
+L_PC_Output_High_Prog_PC4:
+	LDA		P_Temp+8
+	BNE		L_Scankey_Effictive_Prog_To
+	LDA		#5
+	STA		P_Temp+2
+	RTS
+L_PC_Output_High_Prog_PC5:
+	LDA		#6
+	STA		P_Temp+2
+	RTS
+L_Scankey_Effictive_Prog_To:
+	JMP		L_Scankey_Effictive_Prog
